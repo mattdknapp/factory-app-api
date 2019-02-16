03e26914bb655460836516976be64dfeb94ab088
mattdknapp <matthewdknapp@gmail.com>
2019-02-15 19:18:44 -0500

    Add some integrity for websocket updates. WIP

--- eventHandlers/index.js
+++ eventHandlers/index.js
@@ -1,15 +1,27 @@
 const factoryQueries = require('../queries/factories')
 
-const syncFactory = socket => res => {
+const syncFactory = (socket, transactionId) => res => {
+  if (transactionId) {
+    socket.emit('TRANSACTION_SUCCESSFUL', transactionId)
+  }
+
   socket.emit('SYNC_FACTORY', JSON.stringify(res))
 }
 
 const updateFactory = socket => data => {
   const json = JSON.parse(data)
+  const {
+    transactionId
+  } = json
+
   console.log(json)
   factoryQueries.update(json)
     .then(factoryQueries.find)
-    .then(res => syncFactory(socket)(res))
+    .then(res => syncFactory(socket, transactionId)(res))
+    .catch(err => {
+      console.error(err)
+      socket.emit('TRANSACTION_ERROR', transactionId)
+    })
 }
 
 const initHandlers = socket => {
